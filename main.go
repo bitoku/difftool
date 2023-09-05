02dab30d0240ae3d8566e07375d6bfd229dafcce
Ayato Tokubi <atokubi@redhat.com>
2023-09-05 12:00:36 +0100

    add list comparison

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/google/go-cmp/cmp"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/json"
 	"k8s.io/apimachinery/pkg/util/yaml"
@@ -24,7 +26,8 @@ import (
 type Object struct {
 	v1.TypeMeta   `json:",inline"`
 	v1.ObjectMeta `json:"metadata"`
-	Spec          map[string]any `json:"spec"`
+	Spec          map[string]any `json:"spec,omitempty"`
+	Items         []*Object      `json:"items,omitempty"`
 }
 
 func (o *Object) String() string {
@@ -32,7 +35,14 @@ func (o *Object) String() string {
 		return fmt.Sprintf("%s %s %s", o.APIVersion, o.Kind, o.Name)
 	}
 	return fmt.Sprintf("%s %s %s/%s", o.APIVersion, o.Kind, o.Namespace, o.Name)
+}
 
+func UnmarshallUnstructured(u *unstructured.Unstructured, v any) error {
+	rawJson, err := u.MarshalJSON()
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(rawJson, v)
 }
 
 func getResource(apiVersion, kind string, mapper meta.RESTMapper) (schema.GroupVersionResource, error) {
@@ -49,25 +59,81 @@ func getResource(apiVersion, kind string, mapper meta.RESTMapper) (schema.GroupV
 	return mapping.Resource, nil
 }
 
-func check(obj *Object, client dynamic.Interface, resource schema.GroupVersionResource) (string, error) {
+func checkObj(obj *Object, client dynamic.Interface, resource schema.GroupVersionResource) (string, error) {
 	// get the current manifest
 	resp, err := client.
 		Resource(resource).
 		Get(context.Background(), obj.Name, v1.GetOptions{})
 	if errors.IsNotFound(err) {
-		return fmt.Sprintf("%s not found", obj), nil
+		return fmt.Sprintf("- %s is not found\n", obj), nil
 	}
 	if err != nil {
 		return "", err
 	}
-	rawJson, err := resp.MarshalJSON()
-	if err != nil {
-		return "", err
-	}
-	fmt.Println(string(rawJson))
+
 	var curr Object
-	json.Unmarshal(rawJson, &curr)
+	err = UnmarshallUnstructured(resp, &curr)
+	if err != nil {
+		return "", err
+	}
+
 	return cmp.Diff(obj.Spec, curr.Spec), nil
+}
+
+func checkList(obj *Object, client dynamic.Interface, resource schema.GroupVersionResource) (string, error) {
+	resp, err := client.
+		Resource(resource).
+		List(context.Background(), v1.ListOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	m := make(map[string]*Object)
+	checked := make(map[string]bool)
+	for _, i := range resp.Items {
+		curr := new(Object)
+		err = UnmarshallUnstructured(&i, curr)
+		if err != nil {
+			return "", err
+		}
+		m[curr.String()] = curr
+		checked[curr.String()] = false
+	}
+	var presence []string
+	var diffs []string
+	for _, i := range obj.Items {
+		curr, ok := m[i.String()]
+		if !ok {
+			presence = append(presence, fmt.Sprintf("- %s is not found\n", i))
+			continue
+		}
+		checked[curr.String()] = true
+		diff := cmp.Diff(i.Spec, curr.Spec)
+		if diff == "" {
+			continue
+		}
+		diffs = append(diffs, fmt.Sprintf("%s\n%s", i.String(), diff))
+	}
+	for k, v := range m {
+		if !checked[k] {
+			presence = append(presence, fmt.Sprintf("+ %s is found, but not in default\n", v))
+		}
+	}
+	diffs = append(presence, diffs...)
+	return strings.Join(diffs, "\n"), nil
+}
+
+func validateList(obj *Object) bool {
+	if len(obj.Items) == 0 {
+		return true
+	}
+	o := obj.Items[0]
+	for _, v := range obj.Items {
+		if v.APIVersion != o.APIVersion || v.Kind != o.Kind {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
@@ -111,10 +177,10 @@ func main() {
 		panic(err.Error())
 	}
 
-	var diffs []string
 	for _, f := range files {
 		var obj Object
 		content, err := os.ReadFile(filepath.Join(*dir, f.Name()))
+		fmt.Println(f.Name())
 		if err != nil {
 			panic(err.Error())
 		}
@@ -128,20 +194,33 @@ func main() {
 		if err != nil {
 			panic(err.Error())
 		}
-		// get gvr
-		resource, err := getResource(obj.APIVersion, obj.Kind, mapper)
-		if err != nil {
-			panic(err.Error())
-		}
 		//check the diff
-		diff, err := check(&obj, client, resource)
+		var diff string
+		if obj.APIVersion == "v1" && obj.Kind == "List" {
+			if !validateList(&obj) {
+				panic(fmt.Sprintf("%s contains different apiVersion or kind\n", f.Name()))
+			}
+			o := obj.Items[0]
+			resource, err := getResource(o.APIVersion, o.Kind, mapper)
+			if err != nil {
+				panic(err.Error())
+			}
+			diff, err = checkList(&obj, client, resource)
+		} else {
+			resource, err := getResource(obj.APIVersion, obj.Kind, mapper)
+			// get gvr
+			if err != nil {
+				panic(err.Error())
+			}
+			diff, err = checkObj(&obj, client, resource)
+		}
 		if err != nil {
 			panic(err.Error())
 		}
-		diffs = append(diffs, diff)
-	}
-
-	for _, diff := range diffs {
-		fmt.Println(diff)
+		if diff == "" {
+			fmt.Println("No diff\n")
+		} else {
+			fmt.Println(diff)
+		}
 	}
 }
