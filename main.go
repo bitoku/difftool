7208f545559fd4e119a5bcf7c5d7eca06e6a3d4b
Ayato Tokubi <atokubi@redhat.com>
2023-09-11 15:56:08 +0100

    separate logic

--- main.go
+++ main.go
@@ -1,19 +1,14 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
-	"strconv"
-	"strings"
 
 	"github.com/google/go-cmp/cmp"
-	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/json"
 	"k8s.io/apimachinery/pkg/util/yaml"
@@ -22,56 +17,14 @@ import (
 	"k8s.io/client-go/restmapper"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
-	"k8s.io/utils/strings/slices"
+
+	"difftool/pkg/objdiff"
 )
 
 type Target struct {
 	v1.TypeMeta `json:",inline"`
 	Manifest    string   `json:"manifest"`
 	Ignore      []string `json:"ignore"`
-}
-
-type Object struct {
-	v1.TypeMeta   `json:",inline"`
-	v1.ObjectMeta `json:"metadata"`
-	Spec          map[string]any `json:"spec,omitempty"`
-	Items         []*Object      `json:"items,omitempty"`
-}
-
-func (o *Object) String() string {
-	if o.Namespace == "" {
-		return fmt.Sprintf("%s %s %s", o.APIVersion, o.Kind, o.Name)
-	}
-	return fmt.Sprintf("%s %s %s/%s", o.APIVersion, o.Kind, o.Namespace, o.Name)
-}
-
-func ignoreMapEntries(ignoredKeys []string) cmp.Option {
-	filter := func(path cmp.Path) bool {
-		var key []string
-		for _, ps := range path {
-			switch x := ps.(type) {
-			case cmp.MapIndex:
-				key = append(key, x.Key().String())
-			case cmp.SliceIndex:
-				key = append(key, strconv.Itoa(x.Key()))
-			}
-		}
-		// check it naively since ignoredKeys won't be so long,
-		return slices.Contains(ignoredKeys, strings.Join(key, "."))
-	}
-	return cmp.FilterPath(filter, cmp.Ignore())
-}
-
-func compare(a, b *Object, opts ...cmp.Option) string {
-	return cmp.Diff(a.Spec, b.Spec, opts...)
-}
-
-func UnmarshallUnstructured(u *unstructured.Unstructured, v any) error {
-	rawJson, err := u.MarshalJSON()
-	if err != nil {
-		return err
-	}
-	return json.Unmarshal(rawJson, v)
 }
 
 func getResource(apiVersion, kind string, mapper meta.RESTMapper) (schema.GroupVersionResource, error) {
@@ -88,68 +41,15 @@ func getResource(apiVersion, kind string, mapper meta.RESTMapper) (schema.GroupV
 	return mapping.Resource, nil
 }
 
-func checkObj(obj *Object, client dynamic.Interface, resource schema.GroupVersionResource, opts ...cmp.Option) (string, error) {
-	// get the current manifest
-	resp, err := client.
-		Resource(resource).
-		Get(context.Background(), obj.Name, v1.GetOptions{})
-	if errors.IsNotFound(err) {
-		return fmt.Sprintf("- %s is not found\n", obj), nil
-	}
+// if we unmarshall yaml directly, int64 is inferred as float64 somehow,
+// so we convert yaml to json first and then unmarshall it
+func unmarshall(data []byte, v any) error {
+	jsonContent, err := yaml.ToJSON(data)
 	if err != nil {
-		return "", err
+		return err
 	}
-
-	curr := new(Object)
-	err = UnmarshallUnstructured(resp, curr)
-	if err != nil {
-		return "", err
-	}
-
-	return compare(obj, curr, opts...), nil
-}
-
-func checkList(obj *Object, client dynamic.Interface, resource schema.GroupVersionResource, opts ...cmp.Option) (string, error) {
-	resp, err := client.
-		Resource(resource).
-		List(context.Background(), v1.ListOptions{})
-	if err != nil {
-		return "", err
-	}
-
-	m := make(map[string]*Object)
-	checked := make(map[string]bool)
-	for _, i := range resp.Items {
-		curr := new(Object)
-		err = UnmarshallUnstructured(&i, curr)
-		if err != nil {
-			return "", err
-		}
-		m[curr.String()] = curr
-		checked[curr.String()] = false
-	}
-	var presence []string
-	var diffs []string
-	for _, i := range obj.Items {
-		curr, ok := m[i.String()]
-		if !ok {
-			presence = append(presence, fmt.Sprintf("- %s is not found\n", i))
-			continue
-		}
-		checked[curr.String()] = true
-		diff := compare(i, curr, opts...)
-		if diff == "" {
-			continue
-		}
-		diffs = append(diffs, fmt.Sprintf("%s\n%s", i.String(), diff))
-	}
-	for k, v := range m {
-		if !checked[k] {
-			presence = append(presence, fmt.Sprintf("+ %s is found, but not in default\n", v))
-		}
-	}
-	diffs = append(presence, diffs...)
-	return strings.Join(diffs, "\n"), nil
+	err = json.Unmarshal(jsonContent, v)
+	return err
 }
 
 func main() {
@@ -212,34 +112,33 @@ func main() {
 	}
 
 	for _, target := range targets {
-		var obj Object
+		var obj objdiff.Object
 		fmt.Println(filepath.Base(target.Manifest))
 		content, err := os.ReadFile(target.Manifest)
 		if err != nil {
 			panic(err.Error())
 		}
+
+		err = unmarshall(content, &obj)
+		if err != nil {
+			panic(err.Error())
+		}
+
 		resource, err := getResource(target.APIVersion, target.Kind, mapper)
 		if err != nil {
 			panic(err.Error())
 		}
 
-		// if we unmarshall directly from yaml, int64 is inferred as float64 somehow
-		// so we convert yaml to json first and then unmarshall it
-		jsonContent, err := yaml.ToJSON(content)
-		if err != nil {
-			panic(err.Error())
-		}
-		err = json.Unmarshal(jsonContent, &obj)
-		if err != nil {
-			panic(err.Error())
-		}
 		//check the diff
 		var diff string
-		opts := []cmp.Option{ignoreMapEntries(target.Ignore)}
-		if obj.APIVersion == "v1" && obj.Kind == "List" {
-			diff, err = checkList(&obj, client, resource, opts...)
+		opts := []cmp.Option{objdiff.IgnoreMapEntries(target.Ignore)}
+		if err != nil {
+			panic(err.Error())
+		}
+		if obj.IsList() {
+			diff, err = objdiff.CheckList(&obj, client, resource, opts...)
 		} else {
-			diff, err = checkObj(&obj, client, resource, opts...)
+			diff, err = objdiff.CheckObj(&obj, client, resource, opts...)
 		}
 		if err != nil {
 			panic(err.Error())
---
+++ pkg/objdiff/objdiff.go
@@ -0,0 +1,136 @@
+package objdiff
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/google/go-cmp/cmp"
+	"k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/json"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/utils/strings/slices"
+)
+
+type Object struct {
+	v1.TypeMeta   `json:",inline"`
+	v1.ObjectMeta `json:"metadata"`
+	Spec          any       `json:"spec,omitempty"`
+	Data          any       `json:"data,omitempty"`
+	Items         []*Object `json:"items,omitempty"`
+}
+
+type Differ interface {
+	Diff(obj *Object) string
+}
+
+type Diff struct {
+}
+
+func (o *Object) IsList() bool {
+	return o.Items != nil
+}
+
+func (o *Object) String() string {
+	if o.Namespace == "" {
+		return fmt.Sprintf("%s %s %s", o.APIVersion, o.Kind, o.Name)
+	}
+	return fmt.Sprintf("%s %s %s/%s", o.APIVersion, o.Kind, o.Namespace, o.Name)
+}
+
+func IgnoreMapEntries(ignoredKeys []string) cmp.Option {
+	filter := func(path cmp.Path) bool {
+		var key []string
+		for _, ps := range path {
+			switch x := ps.(type) {
+			case cmp.MapIndex:
+				key = append(key, x.Key().String())
+			case cmp.SliceIndex:
+				key = append(key, strconv.Itoa(x.Key()))
+			}
+		}
+		// check it naively since ignoredKeys won't be so long,
+		return slices.Contains(ignoredKeys, strings.Join(key, "."))
+	}
+	return cmp.FilterPath(filter, cmp.Ignore())
+}
+
+func compare(a, b *Object, opts ...cmp.Option) string {
+	return cmp.Diff(a.Spec, b.Spec, opts...)
+}
+
+func unmarshallUnstructured(u *unstructured.Unstructured, v any) error {
+	rawJson, err := u.MarshalJSON()
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(rawJson, v)
+}
+
+func CheckObj(obj *Object, client dynamic.Interface, resource schema.GroupVersionResource, opts ...cmp.Option) (string, error) {
+	// get the current manifest
+	resp, err := client.
+		Resource(resource).
+		Get(context.Background(), obj.Name, v1.GetOptions{})
+	if errors.IsNotFound(err) {
+		return fmt.Sprintf("- %s is not found\n", obj), nil
+	}
+	if err != nil {
+		return "", err
+	}
+
+	curr := new(Object)
+	err = unmarshallUnstructured(resp, curr)
+	if err != nil {
+		return "", err
+	}
+
+	return compare(obj, curr, opts...), nil
+}
+
+func CheckList(obj *Object, client dynamic.Interface, resource schema.GroupVersionResource, opts ...cmp.Option) (string, error) {
+	resp, err := client.
+		Resource(resource).
+		List(context.Background(), v1.ListOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	m := make(map[string]*Object)
+	checked := make(map[string]bool)
+	for _, i := range resp.Items {
+		curr := new(Object)
+		err = unmarshallUnstructured(&i, curr)
+		if err != nil {
+			return "", err
+		}
+		m[curr.String()] = curr
+		checked[curr.String()] = false
+	}
+	var presence []string
+	var diffs []string
+	for _, i := range obj.Items {
+		curr, ok := m[i.String()]
+		if !ok {
+			presence = append(presence, fmt.Sprintf("- %s is not found\n", i))
+			continue
+		}
+		checked[curr.String()] = true
+		diff := compare(i, curr, opts...)
+		if diff == "" {
+			continue
+		}
+		diffs = append(diffs, fmt.Sprintf("%s\n%s", i.String(), diff))
+	}
+	for k, v := range m {
+		if !checked[k] {
+			presence = append(presence, fmt.Sprintf("+ %s is found, but not in default\n", v))
+		}
+	}
+	diffs = append(presence, diffs...)
+	return strings.Join(diffs, "\n"), nil
+}
